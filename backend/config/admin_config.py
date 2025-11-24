e585158567453eca3603c66035080410c6c60fbc
nisha <2016ume1288@mnit.ac.in>
2025-11-24 18:00:08 +0530

    chnage admin config

--- backend/config/admin_config.py
+++ backend/config/admin_config.py
@@ -5,9 +5,9 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     ENV: str = "dev"                # "dev" / "staging" / "prod"
     ENABLE_ADMIN: bool = True      # default False -> admin not loaded
-    ADMIN_SECRET: Optional[str] = None
-    ADMIN_ALLOWLIST_IPS: List[str] = []  # optional
-    TEST_ADMIN_ID : str
+    # ADMIN_SECRET: Optional[str] = None
+    # ADMIN_ALLOWLIST_IPS: List[str] = []  # optional
+    # TEST_ADMIN_ID : str
 
     class Config:
         env_file = ".env"
