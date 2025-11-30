d1ae62a19ea8032c70bf097b74e3f7a3b66633a5
nisha <2016ume1288@mnit.ac.in>
2025-12-01 00:33:36 +0530

    redeploy without cb

--- backend/auth/routes.py
+++ backend/auth/routes.py
@@ -12,7 +12,7 @@ from backend.auth.services import create_user, issue_auth_tokens, logout_device_
 from backend.common.utils import success_response
 from backend.db.dependencies import get_session
 from sqlalchemy.exc import InterfaceError,OperationalError
-from backend.common.circuit_breaker import db_circuit, guard_with_circuit
+# from backend.common.circuit_breaker import db_circuit, guard_with_circuit
 from backend.common.retries import retry_async, is_recoverable_exception
 from backend.config.admin_config import admin_config
 from backend.auth.constants import logger
@@ -119,8 +119,8 @@ async def logout(device_public_id: str = Depends(device_session_pid), session = 
 # -------------------------------------------------------------------------------------------------------------
 
 @auth_router.get("/retries_cb_test")
-@guard_with_circuit(db_circuit)
-@retry_async(attempts=1, base_delay=0.2, factor=2.0, max_delay=5.0, if_retryable=is_recoverable_exception)
+# @guard_with_circuit(db_circuit)
+# @retry_async(attempts=1, base_delay=0.2, factor=2.0, max_delay=5.0, if_retryable=is_recoverable_exception)
 async def health_check(session:AsyncSession=Depends(get_session)):
     stmt=select(1)
     
